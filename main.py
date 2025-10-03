5a67ffe1c8a3f255448c68e49908af17d2c93452
Tim <timmn@mail.ru>
2025-10-03 19:31:33 +0300

    Добавил основной файл mai

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+
+from config import settings
+from bot.handlers import setup_handlers
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    if not settings.bot_token:
+        raise RuntimeError("BOT_TOKEN не задан. Укажи его в .env")
+
+    bot = Bot(token=settings.bot_token, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+
+    setup_handlers(dp)
+
+    logging.info("Бот запущен...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
